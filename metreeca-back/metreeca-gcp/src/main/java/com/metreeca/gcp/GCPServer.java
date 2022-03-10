30d325eb91ad10925409aaffca9738f9f26c8483
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-03-10 12:00:10 +0100

    GPC / Fix GCPServer.development() method / Now relies on PORT env variable

--- metreeca-back/metreeca-gcp/src/main/java/com/metreeca/gcp/GCPServer.java
+++ metreeca-back/metreeca-gcp/src/main/java/com/metreeca/gcp/GCPServer.java
@@ -57,7 +57,7 @@ public final class GCPServer {
      * @return {@code true} if running in the development environment; {@code false}, otherwise
      */
     public static boolean development() {
-		return Objects.isNull(project());
+		return Objects.isNull(System.getenv(AddressVariable));
     }
 
     /**
