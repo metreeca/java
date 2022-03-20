0808fcccc96df6fe730c74e3731ee4c8e2badd5a
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-03-20 09:40:18 +0100

    Core / Define Formats.ISO_LOCAL_DATE_TIME_COMPACT

--- metreeca-core/src/main/java/com/metreeca/core/Formats.java
+++ metreeca-core/src/main/java/com/metreeca/core/Formats.java
@@ -2,9 +2,11 @@ package com.metreeca.core;
 
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.util.Locale;
 
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_TIME;
+import static java.time.temporal.ChronoField.*;
 
 /**
  * Format utilities.
@@ -15,12 +17,30 @@ public final class Formats {
      * A SQL timestamp formatter ({@code yyyy-MM-dd hh:mm:ss}).
      */
     public static final DateTimeFormatter SQL_TIMESTAMP=new DateTimeFormatterBuilder()
+
             .parseCaseInsensitive()
+            .parseStrict()
+
             .append(ISO_LOCAL_DATE)
             .appendLiteral(' ')
             .append(ISO_LOCAL_TIME)
+
+            .toFormatter(Locale.ROOT);
+
+
+    public static final DateTimeFormatter ISO_LOCAL_DATE_TIME_COMPACT=new DateTimeFormatterBuilder()
+
             .parseStrict()
-            .toFormatter();
+
+            .appendValue(YEAR, 4)
+            .appendValue(MONTH_OF_YEAR, 2)
+            .appendValue(DAY_OF_MONTH, 2)
+            .appendLiteral("T")
+            .appendValue(HOUR_OF_DAY, 2)
+            .appendValue(MINUTE_OF_HOUR, 2)
+            .appendValue(SECOND_OF_MINUTE, 2)
+
+            .toFormatter(Locale.ROOT);
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
