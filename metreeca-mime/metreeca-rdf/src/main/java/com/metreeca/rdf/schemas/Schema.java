23cece5db735bf0ca56d7125deab5b4d46049cf9
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-05-30 12:26:17 +0200

    RDF / Simplify Schema API

--- metreeca-mime/metreeca-rdf/src/main/java/com/metreeca/rdf/schemas/Schema.java
+++ metreeca-mime/metreeca-rdf/src/main/java/com/metreeca/rdf/schemas/Schema.java
@@ -19,11 +19,8 @@ package com.metreeca.rdf.schemas;
 import com.metreeca.link.Shape;
 
 import org.eclipse.rdf4j.model.*;
-import org.eclipse.rdf4j.model.vocabulary.RDFS;
-import org.eclipse.rdf4j.model.vocabulary.XSD;
 
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static com.metreeca.link.Shape.multiple;
@@ -32,7 +29,8 @@ import static com.metreeca.link.Values.*;
 import static com.metreeca.link.shapes.And.and;
 import static com.metreeca.link.shapes.Datatype.datatype;
 import static com.metreeca.link.shapes.Field.field;
-import static com.metreeca.link.shapes.Or.or;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Schema.org RDF vocabulary.
@@ -100,215 +98,49 @@ public final class Schema {
 				field(description, labels),
 				field(disambiguatingDescription, labels)
 
-		);
-	}
+        );
+    }
 
 
-	//// Events ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	public enum EventStatus {EventScheduled, EventMovedOnline, EventPostponed, EventRescheduled, EventCancelled}
+    /**
+     * Upgrades legacy {@value #NamespaceLegacy} references.
+     *
+     * @param model the RDF model to be normalized
+     *
+     * @return a normalized {@code model} where legacy {@value #NamespaceLegacy} references are upgraded to  {@value
+     * #Namespace}
+     *
+     * @throws NullPointerException if {@code model} is null or contains null elements
+     */
+    public static Collection<Statement> normalize(final Collection<Statement> model) {
 
-	public static final IRI Event=term("Event");
+        if ( model == null ) {
+            throw new NullPointerException("null model");
+        }
 
-	public static final IRI organizer=term("organizer");
-	public static final IRI isAccessibleForFree=term("isAccessibleForFree");
-	public static final IRI eventStatus=term("eventStatus");
-	public static final IRI location=term("location");
-	public static final IRI eventAttendanceMode=term("eventAttendanceMode");
-	public static final IRI inLanguage=term("inLanguage");
-	public static final IRI audience=term("audience");
-	public static final IRI typicalAgeRange=term("typicalAgeRange");
-	public static final IRI startDate=term("startDate");
-	public static final IRI endDate=term("endDate");
+        return model.stream().map(Schema::normalize).collect(toList());
+    }
 
+    /**
+     * Upgrades legacy {@value #NamespaceLegacy} references.
+     *
+     * @param model the RDF model to be normalized
+     *
+     * @return a normalized {@code model} where legacy {@value #NamespaceLegacy} references are upgraded to  {@value
+     * #Namespace}
+     *
+     * @throws NullPointerException if {@code model} is null or contains null elements
+     */
+    public static Stream<Statement> normalize(final Stream<Statement> model) {
 
-	/**
-	 * Creates an event shape.
-	 *
-	 * @param labels additional constraints for textual labels (e.g. localized names)
-	 *
-	 * @return an event shape including {@code labels} constraints for textual labels
-	 *
-	 * @throws NullPointerException if {@code labels} is nul or contains null elements
-	 */
-	public static Shape Event(final Shape... labels) {
+        if ( model == null ) {
+            throw new NullPointerException("null model");
+        }
 
-		if ( labels == null || Arrays.stream(labels).anyMatch(Objects::isNull) ) {
-			throw new NullPointerException("null labels");
-		}
-
-		return and(Thing(labels),
-
-				field(organizer, optional(),
-						field(RDFS.LABEL, labels)
-				),
-
-				field(isAccessibleForFree, optional(), datatype(XSD.BOOLEAN)),
-				field(eventStatus, optional(), datatype(IRIType)),
-
-				field(location, optional(), or(
-
-						Place(labels),
-						PostalAddress(labels),
-						VirtualLocation(labels)
-
-				)),
-
-				field(eventAttendanceMode, multiple(), datatype(IRIType)),
-
-				field(audience, multiple(),
-						field(RDFS.LABEL, labels)
-				),
-
-				field(inLanguage, multiple(), datatype(XSD.STRING)),
-				field(typicalAgeRange, multiple(), datatype(XSD.STRING)),
-
-				field(startDate, optional(), datatype(XSD.DATETIME)),
-				field(endDate, optional(), datatype(XSD.DATETIME)));
-	}
-
-
-	//// Places ////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	public static final IRI Place=term("Place");
-	public static final IRI PostalAddress=term("PostalAddress");
-	public static final IRI VirtualLocation=term("VirtualLocation");
-
-
-	public static IRI address=term("address");
-
-	public static IRI latitude=term("latitude");
-	public static IRI longitude=term("longitude");
-
-	public static IRI addressCountry=term("addressCountry");
-	public static IRI addressRegion=term("addressRegion");
-	public static IRI addressLocality=term("addressLocality");
-
-	public static IRI postalCode=term("vpostalCode");
-	public static IRI streetAddress=term("streetAddress");
-
-
-	/**
-	 * Creates a location shape.
-	 *
-	 * @param labels additional constraints for textual labels (e.g. localized names)
-	 *
-	 * @return a location shape including {@code labels} constraints for textual labels
-	 *
-	 * @throws NullPointerException if {@code labels} is nul or contains null elements
-	 */
-	public static Shape Location(final Shape... labels) {
-
-		if ( labels == null || Arrays.stream(labels).anyMatch(Objects::isNull) ) {
-			throw new NullPointerException("null labels");
-		}
-
-		return or(
-				Place(labels),
-				VirtualLocation(labels)
-		);
-	}
-
-	/**
-	 * Creates a place shape.
-	 *
-	 * @param labels additional constraints for textual labels (e.g. localized names)
-	 *
-	 * @return a place shape including {@code labels} constraints for textual labels
-	 *
-	 * @throws NullPointerException if {@code labels} is nul or contains null elements
-	 */
-	public static Shape Place(final Shape... labels) {
-
-		if ( labels == null || Arrays.stream(labels).anyMatch(Objects::isNull) ) {
-			throw new NullPointerException("null labels");
-		}
-
-		return and(Thing(labels),
-
-				field(address, optional(), PostalAddress(labels)),
-
-				field(latitude, optional(), datatype(XSD.DECIMAL)),
-				field(longitude, optional(), datatype(XSD.DECIMAL))
-
-		);
-	}
-
-	/**
-	 * Creates a postal address shape.
-	 *
-	 * @param labels additional constraints for textual labels (e.g. localized names)
-	 *
-	 * @return a postal address shape including {@code labels} constraints for textual labels
-	 *
-	 * @throws NullPointerException if {@code labels} is nul or contains null elements
-	 */
-	public static Shape PostalAddress(final Shape... labels) {
-
-		if ( labels == null || Arrays.stream(labels).anyMatch(Objects::isNull) ) {
-			throw new NullPointerException("null labels");
-		}
-
-		return and(Thing(labels),
-
-				field(addressCountry, datatype(XSD.DECIMAL)),
-				field(addressRegion, datatype(XSD.STRING)),
-				field(addressLocality, datatype(XSD.STRING)),
-				field(postalCode, datatype(XSD.STRING)),
-				field(streetAddress, datatype(XSD.STRING))
-
-		);
-	}
-
-	/**
-	 * Creates a virtual location shape.
-	 *
-	 * @param labels additional constraints for textual labels (e.g. localized names)
-	 *
-	 * @return a virtual location shape including {@code labels} constraints for textual labels
-	 *
-	 * @throws NullPointerException if {@code labels} is nul or contains null elements
-	 */
-	public static Shape VirtualLocation(final Shape... labels) {
-
-		if ( labels == null || Arrays.stream(labels).anyMatch(Objects::isNull) ) {
-			throw new NullPointerException("null labels");
-		}
-
-		return and(Thing(labels));
-	}
-
-
-	//// ContactPoints /////////////////////////////////////////////////////////////////////////////////////////////////
-
-	public static final IRI ContactPoint=term("ContactPoint");
-
-
-	public static IRI email=term("email");
-	public static IRI telephone=term("telephone");
-	public static IRI faxNumber=term("faxNumber");
-
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	/**
-	 * Upgrades legacy {@value #NamespaceLegacy} references.
-	 *
-	 * @param model the RDF model to be normalized
-	 *
-	 * @return a normalized {@code model} where legacy {@value #NamespaceLegacy} references are upgraded to  {@value
-	 * #Namespace}
-	 *
-	 * @throws NullPointerException if {@code model} is null or contains null elements
-	 */
-	public static Stream<Statement> normalize(final Stream<Statement> model) {
-
-		if ( model == null ) {
-			throw new NullPointerException("null model");
-		}
-
-		return model.map(Schema::normalize);
-	}
+        return model.map(Schema::normalize);
+    }
 
 	/**
 	 * Upgrades legacy {@value #NamespaceLegacy} references.
