1ea6c327ddb6f03252294d3c13f95c58ab5d6836
Alessandro Bollini <alessandro.bollini@metreeca.com>
2023-01-04 15:25:28 +0100

    RDF4j / Add language filtering to Upload action

--- metreeca-back/metreeca-rdf4j/src/main/java/com/metreeca/rdf4j/actions/Upload.java
+++ metreeca-back/metreeca-rdf4j/src/main/java/com/metreeca/rdf4j/actions/Upload.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013-2022 Metreeca srl
+ * Copyright © 2013-2023 Metreeca srl
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,9 +29,13 @@ import java.util.function.Consumer;
 import static com.metreeca.core.Locator.service;
 import static com.metreeca.core.services.Logger.logger;
 import static com.metreeca.core.services.Logger.time;
+import static com.metreeca.link.Values.lang;
 
 import static java.lang.String.format;
+import static java.util.Comparator.comparing;
+import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toSet;
 
 
 /**
@@ -46,14 +50,15 @@ public final class Upload implements Consumer<Collection<Statement>> {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	private Graph graph=service(Graph.graph());
+    private Graph graph=service(Graph.graph());
 
     private Resource[] contexts=DefaultContexts;
+    private Set<String> langs=Set.of();
 
     private final AtomicBoolean clear=new AtomicBoolean();
     private final AtomicLong count=new AtomicLong();
 
-	private final Logger logger=service(logger());
+    private final Logger logger=service(logger());
 
 
     /**
@@ -96,6 +101,58 @@ public final class Upload implements Consumer<Collection<Statement>> {
         return this;
     }
 
+
+    /**
+     * Configures the languages to be retained.
+     *
+     * <p><strong>Warning</strong> / Untagged literals are always retained.</p>
+     *
+     * @param langs the <a href="https://www.rfc-editor.org/rfc/rfc5646.html">language tags</a> to be retained; if empty,
+     *              all languages are retained
+     *
+     * @return this action
+     *
+     * @throws NullPointerException if {@code langs} is null or contains null values
+     */
+    public Upload langs(final String... langs) {
+
+        if ( langs == null || Arrays.stream(langs).anyMatch(Objects::isNull) ) {
+            throw new NullPointerException("null langs");
+        }
+
+        this.langs=Arrays.stream(langs)
+                .map(lang -> lang.toLowerCase(Locale.ROOT))
+                .collect(toSet());
+
+        return this;
+    }
+
+    /**
+     * Configures the languages to be retained.
+     *
+     * <p><strong>Warning</strong> / Untagged literals are always retained.</p>
+     *
+     * @param langs the <a href="https://www.rfc-editor.org/rfc/rfc5646.html">language tags</a> to be retained; if empty,
+     *              all languages are retained
+     *
+     * @return this action
+     *
+     * @throws NullPointerException if {@code langs} is null or contains null values
+     */
+    public Upload langs(final Collection<String> langs) {
+
+        if ( langs == null || langs.stream().anyMatch(Objects::isNull) ) {
+            throw new NullPointerException("null langs");
+        }
+
+        this.langs=langs.stream()
+                .map(lang -> lang.toLowerCase(Locale.ROOT))
+                .collect(toSet());
+
+        return this;
+    }
+
+
     /**
      * Configures the clear flag (default to {@code false}).
      *
@@ -123,31 +180,69 @@ public final class Upload implements Consumer<Collection<Statement>> {
     @Override public void accept(final Collection<Statement> statements) {
         if ( statements != null && !statements.isEmpty() ) {
 
-	        final String contexts=this.contexts.length == 0 ? "default context" : Arrays.stream(this.contexts)
-			        .map(Value::stringValue)
-			        .collect(joining(", "));
+            final String targets=contexts.length == 0 ? "default context" : Arrays.stream(contexts)
+                    .map(Value::stringValue)
+                    .collect(joining(", "));
 
-	        graph.update(connection -> time(() -> {
+            graph.update(connection -> time(() -> {
 
-		        if ( clear.getAndSet(false) ) {
+                if ( clear.getAndSet(false) ) {
 
-			        connection.clear(this.contexts);
+                    connection.clear(contexts);
 
-			        logger.info(this, format(
-					        "cleared <%s>", contexts
-			        ));
-		        }
+                    logger.info(this, format(
+                            "cleared <%s>", targets
+                    ));
 
-		        if ( !statements.isEmpty() ) {
-			        connection.add(statements, this.contexts);
-		        }
+                }
 
-	        }).apply(t -> logger.info(this, format(
-			        "uploaded <%,d / %,d> statements to <%s> in <%,d> ms",
-			        statements.size(), count.addAndGet(statements.size()), contexts, t
-	        ))));
+                if ( statements instanceof NamespaceAware ) {
+                    ((NamespaceAware)statements).getNamespaces().stream()
+
+                            .filter(not(namespace -> namespace.getPrefix().isEmpty()))
+
+                            .sorted(comparing(Namespace::getPrefix))
+
+                            .peek(entry -> logger.info(this, format(
+                                    "%-8s %s", entry.getPrefix(), entry.getName()
+                            )))
+
+                            .forEach(namespace -> connection.setNamespace(namespace.getPrefix(), namespace.getName()));
+                }
+
+                if ( !statements.isEmpty() ) {
+                    connection.add(
+
+                            langs.isEmpty()
+                                    ? statements
+                                    : () -> statements.stream().filter(this::retain).iterator(),
+
+                            contexts
+
+                    );
+                }
+
+            }).apply(t -> logger.info(this, format(
+                    "uploaded <%,d / %,d> statements to <%s> in <%,d> ms",
+                    statements.size(), count.addAndGet(statements.size()), targets, t
+            ))));
 
         }
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private boolean retain(final Statement statement) {
+        return retain(statement.getObject());
+    }
+
+    private boolean retain(final Value value) {
+        return retain(lang(value));
+    }
+
+    private boolean retain(final String lang) {
+        return lang.isEmpty() || langs.contains(lang);
     }
 
 }
